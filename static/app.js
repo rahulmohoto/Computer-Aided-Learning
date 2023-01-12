77408df5e1e3fd2b3501b6dce53d4938f99f1514
rahul mohoto <rahulmohoto@gmail.com>
2023-01-12 17:21:30 +0600

    modified app.js

--- static/app.js
+++ static/app.js
@@ -4,7 +4,8 @@ const App = {
             counter: 8,
             // isActive: true,
             // isClass2: false,
-            canvas: "canvas",
+            canvas: null,
+            canvasImg: null,
             ctx: false,
             flag: false,
             prevX: 0,
@@ -70,6 +71,15 @@ const App = {
                     this.dot_flag = false;
                 }
             }
+        },
+
+        erase() {
+            var m = confirm("Want to clear");
+            if (m) {
+                this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+                this.canvasImg.style.display = "none";
+                // document.getElementById("canvasimg").style.display = "none";
+            }
         }
     },
 
@@ -78,14 +88,8 @@ const App = {
     },
 
     mounted() {
-        // canvas = document.getElementById('can');
         this.canvas = this.$refs.canvas;
-        // console.log(canvas);
         this.ctx = this.canvas.getContext("2d");
-        w = this.canvas.width;
-        h = this.canvas.height;
-
-        // this.initialize();
     }
 }
 
